9e95d05117a386efa58a7ba95a4853ab4f0c4ce0
ikaslea <ikaslea@DESKTOP-K6TLILG>
2020-11-18 14:08:18 +0100

    2

--- menuakgit/src/menuakgit/menuagit.java
+++ menuakgit/src/menuakgit/menuagit.java
@@ -4,7 +4,15 @@ public class menuagit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		System.out.println("1. opcion1");
+		System.out.println("2. opcion2");
+		System.out.println("3. opcion3");
+		System.out.println("4. opcion4");
+		
+	
+	
+	
 	}
 
 }
